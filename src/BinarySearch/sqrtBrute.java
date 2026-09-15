4b67813cde4aff60991ff3344a2a8efbe0ea3ec9
Zubair Beigh <beighzubair35@gmail.com>
2026-09-15 16:21:44 +0530

    Revision

--- src/BinarySearch/sqrtBrute.java
+++ src/BinarySearch/sqrtBrute.java
@@ -27,10 +27,26 @@ public class sqrtBrute {
         }
         return ans;
     }
+    static int maxInteger(int n){
+        int low=1;
+        int high=n;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((long)mid*mid<=n){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
 
     static void main(String[] args) {
         int n=27;
         System.out.println(floorSqrtBrute(n));
         System.out.println(floorSqrt(n));
+        System.out.println(maxInteger(n));
     }
 }
