92e367d8701f0a6cf43e33a7da46cac260a00a44
Zubair Beigh <beighzubair35@gmail.com>
2026-09-15 16:15:31 +0530

    Revision

---
+++ src/BinarySearch/sqrtBrute.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+public class sqrtBrute {
+    static int floorSqrtBrute(int n){
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if((long)i*i<=n){
+                ans=i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+    static int floorSqrt(int n){
+        int low=1;
+        int high=n;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((long)mid*mid<=n){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    static void main(String[] args) {
+        int n=27;
+        System.out.println(floorSqrtBrute(n));
+        System.out.println(floorSqrt(n));
+    }
+}
